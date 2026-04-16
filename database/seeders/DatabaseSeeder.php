4b72e94feef91577930194c89b257923a1b13a7f
Kevin Gallarin <gallarinkevinm@gmail.com>
2026-04-17 04:11:55 +0800

    remove media-library:clean from seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,14 +3,11 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\Artisan;
 
 class DatabaseSeeder extends Seeder
 {
     public function run(): void
     {
-        Artisan::call('media-library:clean');
-
         $this->call([
             ContentSeeder::class,
             RecommendationSeeder::class,
